cf67da94e3641f0c494cbee732cc0a8963c17cce
ragil <tripleone92@gmail.com>
2019-09-19 20:29:47 +0700

    Interaksi dengan Back-End, Pemanggilan API (POST) Part 6 - #9-6 ReactJS Tutorial [Indonesia]

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,7 +5,55 @@ import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
-        post: []
+        post: [],
+        formBlogPost:{
+            id:1,
+            title:'',
+            body:'',
+            userId:1
+        }
+    }
+
+    getPostAPI = () => {
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
+        .then((result)=>{
+            //console.log(result.data);
+            this.setState({
+                post:result.data
+            })
+        })
+    }
+
+    postDataToAPI = () => {
+        axios.post('http://localhost:3004/posts/', this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+        }, (err) => {
+            console.log('error: ', err);
+            
+        })
+    }
+
+    handleRemove = (data) =>{        
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+            this.getPostAPI();
+        })
+    }
+
+    handleFormChange = (event) => {        
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        formBlogPostNew['id'] = timestamp;        
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+   
+    }
+
+    handleSubmit = () => {
+        this.postDataToAPI();
+        
     }
     
     componentDidMount(){
@@ -18,23 +66,24 @@ class BlogPost extends Component {
         //         post: json
         //     })
         // })
-        axios.get('http://localhost:3004/posts')
-        .then((result)=>{
-            //console.log(result.data);
-            this.setState({
-                post:result.data
-            })
-        })
+        this.getPostAPI();
     }
 
     render() {
         return (
             <Fragment>
-                <div className="wrapContent">
-                    <p>BlogPost</p>    
+                <p>BlogPost</p>   
+                <div className="form-add-post">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" placeholder="add text" onChange={this.handleFormChange}/>
+                    <label htmlFor="body"> Blog Content</label>
+                    <textarea name="body" id="body" cols="30" rows="10" placeholder="add content" onChange={this.handleFormChange}></textarea>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
+                </div>
+                <div className="wrapContent">                     
                     {
                         this.state.post.map(post =>{
-                            return <Post key={post.id} title={post.title} desc={post.body}/>     
+                            return <Post key={post.id} data={post} remove={this.handleRemove}/>     
                         })
                     }
                                
